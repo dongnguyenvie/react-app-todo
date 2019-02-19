349616444cdbf4f5b6e6cb428386d6dc8c06494d
DongNguyen <dongnguyenvie@gmail.com>
2019-02-20 01:55:55 +0700

    app todo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import TodoItems from './components/TodoItems'
-
+import tick from './components/img/tick.svg'
 class App extends Component {
   constructor () {
     super()
@@ -19,9 +19,12 @@ class App extends Component {
           title: 'Play game',
           status: false
         }
-      ]
+      ],
+      newItem: ''
     }
-    this.onItemClicked = this.onItemClicked.bind(this)
+    //this.onItemClicked = this.onItemClicked.bind(this)
+    this.onKeyUp = this.onKeyUp.bind(this)
+    this.onChange = this.onChange.bind(this)
   } // contructor
 
   onItemClicked(item) {
@@ -43,11 +46,43 @@ class App extends Component {
       })
       console.log(this.state.todoItems)
     }
+  }// on click
+
+  onKeyUp(event) {
+    if(event.keyCode == 13) {
+      let text = event.target.value
+      console.log(text)
+      if(!text) return;
+      text = text.trim()
+      if(!text) return;
+      this.setState({
+        todoItems: [
+          {title: text, status: false },
+          ...this.state.todoItems
+        ]
+      })
+      this.setState({
+        newItem: ''
+      })
+    }
+  }
+
+  onChange(event){
+    this.setState({
+      newItem: event.target.value
+    })
   }
 
   render() {
     return (
       <div className="App">
+        <div className="header">
+          <img src={tick} width={32} height="32" />
+          <input type="text" placeholder="Add new item" 
+            value={this.state.newItem}
+            onChange={this.onChange}
+             onKeyUp={this.onKeyUp} />
+        </div>
         {
           this.state.todoItems.length > 0 &&
           this.state.todoItems.map( (item, index) => 
--- src/components/TodoItems.js
+++ src/components/TodoItems.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
 import './TodoItems.css'
+import checkImg from './img/checked.svg'
+import checkComplete from './img/check-complete.svg'
+import classNames from 'classnames'
+
 class TodoItems extends Component{
 
     render () {
         const { item, onClick } = this.props
-        let className = 'TodoItem'
-        if( item.status ) {
-            className += ' Todo-Compolete'
+        let url = checkImg
+        if(item.status) {
+            url = checkComplete
         }
         return (
-            <div className={className} onClick={ onClick } >
+            <div className={classNames('TodoItem', {
+                'Todo-Compolete': item.status
+            })} onClick={ onClick } >
+                <img src={url} width={32} height={32} />
                 <p>{ item.title }</p>
             </div>
         );
